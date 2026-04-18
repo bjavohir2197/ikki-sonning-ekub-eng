2a3285e1c09adc66b76e7c77a6534053c98c8035
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 12:42:19 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Ikki sonning EKUB (Eng Katta Umumiy Bo‘luvchisi) ni toping (Evklid algoritmi yordamida).")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
